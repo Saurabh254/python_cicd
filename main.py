74e4befb8def143b342a8ad4f843b04f9abe71e8
Saurabh <sauravvishwakarma030@gmail.com>
2024-08-02 22:43:22 +0530

    fix

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+"""This is main moudle where our intial files will be placed
+    """
+
+
 def runner():
+    """runner function to return a value
+
+    Returns:
+        int: returns an integer value
+    """
     return 0
