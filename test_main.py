7f32fe5c76f77310cbad92c176e1fea278e9c88f
Saurabh <sauravvishwakarma030@gmail.com>
2024-08-02 22:45:28 +0530

    fix

--- test_main.py
+++ test_main.py
@@ -1,6 +1,10 @@
+"""this is the test file for the main module
+    """
+
 from main import runner
 
 
 def test_random_func():
+    """this the test funtion to tes tthe our package from random module"""
     val = runner()
     assert val == 0
